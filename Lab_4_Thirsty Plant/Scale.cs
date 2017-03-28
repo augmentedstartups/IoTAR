853ed97f43ce56720d5c252fdb858a77d6e1832f
Arduino Startups <reigngt09@gmail.com>
2017-03-28 16:49:39 +0200

    Add files via upload

---
+++ Lab_4_Thirsty Plant/Scale.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scale : MonoBehaviour {
+
+	public float size = 5f;
+
+	Vector3 temp;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () { 
+		temp =	transform.localScale;
+
+		temp.x = size;
+
+		transform.localScale = temp;
+	}
+
+	public void	AdjustSize(float newSize){
+		size = newSize;
+	
+	
+	}
+}
