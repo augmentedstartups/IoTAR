8a89f429fb74f95589a0e0c2a39ccfcd0787acd4
Arduino Startups <reigngt09@gmail.com>
2017-04-02 13:10:10 +0200

    Add files via upload

--- Lab_4_Thirsty Plant/Photon/ThirstyPlant_Photon.ino
+++ Lab_4_Thirsty Plant/Photon/ThirstyPlant_Photon.ino
@@ -1,12 +1,12 @@
 //Created By: Ritesh Kanjee
-//Project   : AR Thirsty Plant
+//Project   : AR Thirsty Plant Part 2
 //Date      : 31st April 2017
 //Website   : www.ArduinoStartups.com
 
 int t = 2000; // time delay
 // Pins
 int WaterLevel_pin = A0;
-
+int WaterLevel_Calibrated;
 
 void setup() {
    
@@ -15,8 +15,15 @@ void setup() {
 void loop() {
     // Water level Measurement
     int WaterLevel = analogRead(WaterLevel_pin);
+    WaterLevel_Calibrated = map(WaterLevel, 2210, 3220, 0, 100); 
+    
     
     // Publish data
-    Particle.publish("waterlevel", String(WaterLevel));// Publish Variable to Cloud);
+    //Particle.publish("waterlevel", String(WaterLevel));// Publish Variable to Cloud);
+    Particle.publish("waterlevel", String(WaterLevel_Calibrated));// Publish Variable to Cloud);
     delay(t);
 }
+
+
+
+
